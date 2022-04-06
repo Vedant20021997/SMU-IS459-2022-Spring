871fe6283783ed70424794a558771719342f3b19
Vedant20021997 <vedantm.2018@smu.edu.sg>
2022-04-06 17:44:31 +0800

    yeap

--- spark/assignment_2.py
+++ spark/assignment_2.py
@@ -1,13 +1,16 @@
+import nltk
+from nltk.corpus import stopwords
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import monotonically_increasing_id
-from pyspark.sql.functions import desc
+from pyspark.sql.functions import desc, lower, regexp_replace, explode
+from pyspark.ml.feature import Tokenizer, StopWordsRemover
 from graphframes import *
 
 spark = SparkSession.builder.appName('sg.edu.smu.is459.assignment2').getOrCreate()
 
 # Load data
-posts_df = spark.read.load('/user/zzj/parquet-input/hardwarezone.parquet')
+posts_df = spark.read.load('/quiz2/spark/hardwarezone.parquet')
 
 # Clean the dataframe by removing rows with any null value
 posts_df = posts_df.na.drop()
@@ -22,7 +25,7 @@ print('# of topics: ' + str(posts_df.select('topic').distinct().count()))
 
 author_df = posts_df.select('author').distinct()
 
-print('Author number :' + str(author_df.count()))
+print('Author number: ' + str(author_df.count()))
 
 # Assign ID to the users
 author_id = author_df.withColumn('id', monotonically_increasing_id())
@@ -41,7 +44,7 @@ author_to_author = left_df. \
     join(right_df, left_df.ltopic == right_df.rtopic) \
     .select(left_df.src_author, right_df.dst_author)
 edge_num = author_to_author.count()
-print('Number of edges with duplicate : ' + str(edge_num))
+print('Number of edges with duplicate: ' + str(edge_num))
 
 # Convert it into ids
 id_to_author = author_to_author \
@@ -64,7 +67,7 @@ id_to_id = id_to_id.filter(id_to_id.n >= 5)
 
 #id_to_id.cache()
 
-print("Number of edges without duplciate :" + str(id_to_id.count()))
+print("Number of edges without duplciate: " + str(id_to_id.count()))
 
 # Build graph with RDDs
 graph = GraphFrame(author_id, id_to_id)
@@ -72,7 +75,34 @@ graph = GraphFrame(author_id, id_to_id)
 # For complex graph queries, e.g., connected components, you need to set
 # the checkopoint directory on HDFS, so Spark can handle failures.
 # Remember to change to a valid directory in your HDFS
-spark.sparkContext.setCheckpointDir('/user/zzj/spark-checkpoint')
+spark.sparkContext.setCheckpointDir('/quiz2/spark/spark-checkpoint')
 
 # The rest is your work, guys
 # ......
+# How large are the communities?
+result = graph.connectedComponents()
+#result.show()
+
+print("Number of connected components: " + str(result.select("component").distinct().count()))
+
+result.write.csv('/quiz2/spark/cc.csv', mode='overwrite')
+
+# What are the key words of the community?
+content = posts_df.select(lower(regexp_replace('content', "[^a-zA-Z\\s]", "")).alias('content'))
+tokenizer = Tokenizer(outputCol="words")
+tokenizer.setInputCol("content")
+df = tokenizer.transform(content)
+list_of_stopwords = list(stopwords.words('english')) + ['', 'u']
+remover = StopWordsRemover(stopWords=list_of_stopwords)
+remover.setInputCols(["words"]).setOutputCols(["cleaned_words"])
+df2 = remover.transform(df)
+df2.withColumn('word', explode('cleaned_words')).groupBy('word').count().sort('count', ascending=False).show()
+
+# How cohesive are the communities?
+result = graph.triangleCount()
+#result.show()
+result_rdd = result.rdd
+map_count = result_rdd.map(lambda x: (x[0], 1))
+(sum, count) = map_count.reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+print(sum, count)
+print("Number of triangle counts: " + str(sum/count))
--- spark/rdd_exercise.py
+++ spark/rdd_exercise.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('RDD Exercise').getOrCreate()
 
 # Load CSV file into a data frame
-score_sheet_df = spark.read.load('/user/zzj/score-sheet.csv', \
+score_sheet_df = spark.read.load('/quiz2/spark/score-sheet.csv', \
     format='csv', sep=';', inferSchema='true', header='true')
 
 score_sheet_df.show()
@@ -15,14 +15,20 @@ score_sheet_rdd.first()
 
 # Project the second column of scores with an additional 1
 score_rdd = score_sheet_rdd.map(lambda x: (x[1], 1))
+df3 = score_rdd.toDF()
+df3.show()
 score_rdd.first()
 
 # Get the sum and count by reduce
+max_score = score_rdd.max()[0]
+min_score = score_rdd.min()[0]
+score_rdd = score_rdd.filter(lambda x: x[0] < max_score and x[0] > min_score)
+# score_rdd = score_rdd.filter(lambda x, y: x if x[0] < y[0] else y)
 (sum, count) = score_rdd.reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]))
 print('Average Score : ' + str(sum/count))
 
 # Load Parquet file into a data frame
-posts_df = spark.read.load('/user/zzj/hardwarezone.parquet')
+posts_df = spark.read.load('/quiz2/spark/hardwarezone.parquet')
 
 posts_df.createOrReplaceTempView("posts")
 sqlDF = spark.sql("SELECT * FROM posts WHERE author='SG_Jimmy'")
@@ -32,9 +38,15 @@ print('Jimmy has ' + str(num_post) + ' posts.')
 posts_rdd = posts_df.rdd
 
 # Project the author and content columns
-author_content_rdd = posts_rdd.map(lambda x: (len(x[2]), 1))
+author_content_rdd = posts_rdd.map(lambda x: (x[1], (len(x[2]), 1)))
+df = author_content_rdd.toDF()
+df.show()
 author_content_rdd.first()
 
 # Get sume and count by reduce
-(sum, count) = author_content_rdd.reduce(lambda x,y: (x[0]+y[0], x[1]+y[1]))
-print('Average post length : ' + str(sum/count))
+grouped = author_content_rdd.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])).map(lambda x: (x[0], x[1][0] / x[1][1]))
+df1 = grouped.toDF()
+df1.show()
+# (sum, count) = author_content_rdd.reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+# print('Average post length : ' + str(sum/count))
+
